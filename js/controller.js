336ed96c669317ed9addf65a54205fea87df5415
eylon697 <84246535+eylon697@users.noreply.github.com>
2021-06-09 16:38:25 +0300

    Second upload

---
+++ js/controller.js
@@ -0,0 +1,45 @@
+'use strict'
+
+function onImageClick() {
+    resetCanvas()
+    toggleGallery()
+    toggleGenerator()
+    renderCanvas()
+    drawText()
+}
+
+function resetCanvas() {
+    gCanvas = document.querySelector('canvas')
+    gCtx = gCanvas.getContext('2d')
+}
+
+function toggleGallery() {
+    var elGallery = document.querySelector('main')
+    elGallery.hidden = !elGallery.hidden
+}
+
+function toggleGenerator() {
+    var elGallery = document.querySelector('.generator')
+    elGallery.hidden = !elGallery.hidden
+}
+
+function renderCanvas() {
+    loadImg()
+}
+
+function loadImg() {
+    var img = new Image()
+    img.src = getImageUrl();
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    }
+}
+
+function drawText() {
+    gCtx.font = "30px Arial";
+    gCtx.fillText("Hello World", 10, 50);
+}
+
+function renderCategories() {
+
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,39 @@
-console.log();
+'use strict'
+
+
+var gCanvas
+var gCtx
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: 'I never eat Falafel',
+        size: 20,
+        align: 'left',
+        color: 'red'
+    }]
+}
+
+var gKeywords = {
+    'happy': 1,
+    'funny': 1
+}
+
+var gImgs = [{
+        id: 1,
+        url: 'img/1.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 2,
+        url: 'img/2.jpg',
+        keywords: ['funny']
+    },
+];
+
+
+
+function getImageUrl() {
+    return gImgs.find(img => img.id === gMeme.selectedImgId).url
+}
